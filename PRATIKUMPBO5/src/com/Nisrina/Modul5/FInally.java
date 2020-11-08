d8b6d6bd6ae022c745daa345936c2e4dd26c080f
Nisrinaeka <72739696+Nisrinaeka@users.noreply.github.com>
2020-11-08 20:35:49 +0700

    Add files via upload

---
+++ PRATIKUMPBO5/src/com/Nisrina/Modul5/FInally.java
@@ -0,0 +1,19 @@
+package com.Nisrina.Modul5;
+
+
+public class Finally {
+    public static void main(String[] args) {
+        int[]array=new int[5];
+        try{
+            //database di open
+            System.out.println("Akses Elemen ke-5\t: "+array[5]);
+        }catch (ArrayIndexOutOfBoundsException e){
+            //Exception
+            System.out.println("Ada Exception Array");
+        }finally {
+            //database di tutup
+            array[array.length -1]=10;
+            System.out.println("Nilai elemen terakhir\t: "+array[array.length -1]);
+        }
+    }
+}
---
+++ PRATIKUMPBO5/src/com/Nisrina/Modul5/Tugas5/InputID.java
@@ -0,0 +1,80 @@
+package com.Nisrina.Modul5.Tugas5;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class InputID {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+        // Deklarasi array dan inisiasi panjang array 10
+        int[] array = new int[10];
+
+        System.out.println("**** Pelanggan Toko Berkah Jaya ****");
+        // Input jumlah pengunjung toko Berkah Jaya
+        System.out.print("Jumlah Pengunjung Toko\t: ");
+        int jmlPengunjung = scan.nextInt();
+
+
+        /**
+         * Menginputkan Maks 10 pengunjung toko
+         * Jika menginputkan jumlah pengunjung toko > 10 maka akan error,
+         * maka perintah catch "Maksimal Jumlah Pengunjung 10" akan di jalankan
+         */
+        System.out.println("Masukkan Data Pengunjung\t: ");
+        for (int i = 0; i < jmlPengunjung ; i++) {
+            try {
+                System.out.print("UID\t: ");
+                array[i] = intInputMethod();
+                System.out.println("Data ke-" + i + " : " + array[i] + " terdaftar");
+
+                // Pernyataan disini akan di eksekusi jika terjadi Exception saat pengisian UID
+            } catch (InputMismatchException e) {
+                System.out.println("Inputkan Angka !!!");
+                i--;
+
+                // Pernyataan disini akan di eksekusi jika terjadi Exception saat jumlah pengunjung lebih dari 10
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Maksimal Jumlah Pengunjung 10");
+            }
+        }
+
+
+        System.out.println();
+        // Lihat data pengunjung toko Berkah Jaya
+        System.out.println("Lihat Data Pengunjung\t: ");
+        // Input indeks untuk mencari UID pengunjung
+        System.out.print("Indeks\t: ");
+        int lihatData = scan.nextInt();
+        System.out.println("Data ke-" + lihatData + " : " + array[lihatData]);
+
+        // Perulangan untuk melihat data indeks lagi
+        boolean cek = false;
+        do {
+            // input jika ingin melihat lagi atau tidak
+            System.out.print("Lihat Lagi? (y/n)\t: ");
+            String lihat = scan.next();
+            // Jika YA maka akan mengeksekusi program ini
+            if (lihat.equals("y") || lihat.equals("Y")) {
+                System.out.println("Lihat Data Pengunjung\t: ");
+                System.out.print("Indeks\t: ");
+                lihatData = scan.nextInt();
+                System.out.println("Data ke-" + lihatData + " : " + array[lihatData]);
+
+                // Jika tidak
+            }else {
+                cek = true;
+            }
+        }while (!cek);
+
+    }
+
+    // Method intInputMethod
+    public static int intInputMethod () {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        return input;
+    }
+
+}
+
